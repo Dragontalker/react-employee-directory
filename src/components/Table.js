77d2c6ce289fdaf321f1151034f606779c4fb1cb
Richard <richard.yang.tong@gmail.com>
2021-03-19 19:18:13 -0400

    Render the contents in headers.

--- src/components/Table.js
+++ src/components/Table.js
@@ -23,7 +23,9 @@ export const Table = (props) => {
                     {headerGroups.map(headerGroup => (
                         <tr {...headerGroup.getHeaderGroupProps()}>
                             {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}></th>
+                                <th {...column.getHeaderProps()}>
+                                    {column.render('Header')}
+                                </th>
                             ))}
                         </tr>
                     ))}
