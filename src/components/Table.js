e76a1b82c39cb6c6141128fb3d03da7239db3132
Richard <richard.yang.tong@gmail.com>
2021-03-19 19:06:11 -0400

    Hooked props.columns using useMemo.

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,8 @@ import React, { useMemo } from 'react';
 import { useTable } from 'react-table';
 
 export const Table = (props) => {
+    const columns = useMemo(() => props.columns, []);
+
     return (
         <div>
 
