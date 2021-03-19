51dff8ee394ba7394bdc5ddd20673f41294b3eb5
Richard <richard.yang.tong@gmail.com>
2021-03-19 09:17:55 -0400

    Added the basic fethcing parameters.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const getApiData = (number, nation) => {
-
+    axios.get('https://randomuser.me/api', {
+        params: {
+            result: number,
+            nat: nation
+        }
+    })
 };
 
 export default getApiData;
