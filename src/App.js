b3bb23acd430ee0ff17c0bdd9a3c751ce663a55d
Richard <richard.yang.tong@gmail.com>
2021-03-19 10:01:31 -0400

    Restored main react to original state.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import getApiData from './utils/API';
+
 
 function App() {
-  getApiData(200, 'us', 'name').then(res => {
-    console.log(res[0]);
-  });
-
-
-
-
   return (
     <div>
       Hellow World!
