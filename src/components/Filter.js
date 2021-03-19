b40b4961c0091ee77a39ec836b9691d5608f27b8
Richard <richard.yang.tong@gmail.com>
2021-03-19 19:34:15 -0400

    Added an input element.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const Filter = ({filter, setFilter}) => {
     return (
         <span>
-            
+            <input
+
+            />
         </span>
     )
 };
