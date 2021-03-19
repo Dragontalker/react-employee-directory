0e7f740efdf2f4d70c3f6c5fc0796f9124ce434f
Richard <richard.yang.tong@gmail.com>
2021-03-19 10:04:01 -0400

    Added a class component for testing api.

---
+++ src/components/DataTest.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import API from '../utils/API';
+
+class DataTest extends Component {
+    state = {
+        search: "",
+        results: []
+    };
+
+};
+
+export default DataTest;
