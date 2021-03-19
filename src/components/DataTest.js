ff8b7f91d0c0248c53bb648819bcd39d0b8aece7
Richard <richard.yang.tong@gmail.com>
2021-03-19 18:20:15 -0400

    Cleaning up and ready to move on.

--- src/components/DataTest.js
+++ src/components/DataTest.js
@@ -4,12 +4,27 @@ import API from '../utils/API';
 class DataTest extends Component {
     state = {
         search: "",
-        results: []
+        results: [],
+        name: ""
     };
+
+    componentDidMount() {
+        this.searchUser('?nat=us')
+    }
+
+    searchUser = query => {
+        API.search(query)
+            .then(res => this.setState({ results: res.data.results[0].name}))
+            .catch(err => console.log(err));
+    }
+
+
 
     render() {
         return (
-            <div></div>
+            <div>
+                The results are: {this.state.name}
+            </div>
         )
     }
 
